7bc18b8909083c3ab4ede088718aca44c252b5d3
ky.hwang <ky.hwang@etribe.co.kr>
2023-05-23 15:13:26 +0900

    코드미리보기 - 아이프레임으로 가능하게 기능 추가

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,23 @@
+var headTag;
+
 window.addEventListener('DOMContentLoaded', (event) => {
+  const setHeadTag = function() {
+    headTag = `<meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="format-detection" content="telephone=no">
+      <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+      <meta charset="utf-8">
+      <title>프로젝트타이틀</title>
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta property="og:title" content="">
+      <meta property="og:url" content="">
+      <meta property="og:image" content="">
+      <meta property="og:description" content="">
+      <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+      <link href="../../static/css/common.css" type="text/css" rel="stylesheet">
+      <script src="../../static/js/jquery.js"></script>`
+  }
+  setHeadTag();
   $('.ui_btnCodeCopy').on('click', function(){
     var el = $(this);
     try {
@@ -37,22 +56,52 @@ window.addEventListener('DOMContentLoaded', (event) => {
         mode: 'text/css',
         tabMode: 'indent'
       });
-    } else if($(this).hasClass('html') === true) {
+    }
+    if($(this).hasClass('javascript')) {
+      var editor = CodeMirror.fromTextArea(elem, {
+          mode: 'text/javascript',
+          tabMode: 'indent'
+      });
+    }
+    if($(this).hasClass('html')) {
       var delay;
       var editor = CodeMirror.fromTextArea(elem, {
         mode: 'text/html',
+        autoRefresh: true,
         tabMode: 'indent'
       });
-      
-      editor.on("change", function() {
-        clearTimeout(delay);
-        delay = setTimeout(updatePreview, 300);
+    }
+    if($(this).hasClass('iframe')) {
+      var delay;
+      var editor = CodeMirror.fromTextArea(elem, {
+          mode: 'text/html',
       });
-      function updatePreview() {
+    }  
+    editor.on("change", function() {
+      clearTimeout(delay);
+      delay = setTimeout(updatePreview, 1);
+    });
+    function updatePreview() {
+      if($(elem).hasClass('iframe')) {
+        var preview = $this.prev().prev();
+        var iframes = $(preview).get(0).contentDocument;
+        console.log(iframes)
+        $("head", iframes).html(headTag);
+        $("body", iframes).html(editor.getValue());
+      }
+      if($(elem).hasClass('html')) {
         var preview = $this.prev().prev();
         preview.html(editor.getValue());
       }
-      setTimeout(updatePreview, 300);
+      if(editor) {
+        editor.refresh();
+      }
     }
+    setTimeout(updatePreview, 1);
   });
 })
+
+$('.CodeMirror').each(function(i,  el){
+  console.log(el)
+  el.CodeMirror.refresh();
+});
